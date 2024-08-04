74395f7609eed016dd7a564fbb02eaca98da6dd3
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-08-04 19:34:29 +0530

    Refactor update method to return updated object;

--- src/main/java/edu/opl/backend/service/CommonService.java
+++ src/main/java/edu/opl/backend/service/CommonService.java
@@ -3,14 +3,14 @@ package edu.opl.backend.service;
 import java.util.List;
 
 public sealed interface CommonService<T, V>
-        permits AdminService, CourseService, InstructorService, StudentService {
+        permits AdminService, AssignmentService, CourseService, InstructorService, StudentService {
     T create(T t);
 
     T findById(final V v);
 
     List<T> findAll();
 
-    void update(final T t);
+    T update(final T t);
 
     void delete(final T t);
 }
